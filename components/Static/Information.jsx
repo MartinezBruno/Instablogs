02f291d86a8242d062875647fbdb420ad956b3bf
Bruno Martinez <bmartinez.fullstack@gmail.com>
2024-09-07 16:38:21 -0300

    fix(information): update container style to fit page width

--- components/Static/Information.jsx
+++ components/Static/Information.jsx
@@ -1,14 +1,16 @@
 const Information = ({ article }) => {
   return (
-    <section className='c-container !pt-36 flex lg:flex-row self-end flex-col max-w-[442px] md:max-w-[650px] lg:max-w-none lg:gap-28 scroll-mt-[104px]'>
+    <section className='c-container !pt-36 flex lg:flex-row self-end flex-col max-w-[442px] lg:gap-28 scroll-mt-[104px]'>
       <img
         src={article.image}
-        alt='Article cover image'
+        alt='Article cover'
         loading='eager'
         className='object-cover w-full lg:max-w-[44%] h-full'
       />
       <div className='basis-full'>
-        <h2 className='text-xl lg:text-5xl font-extrabold text-yellow dark:text-purple my-3'>{article.title}</h2>
+        <h2 className='text-xl lg:text-5xl font-extrabold text-yellow dark:text-purple my-3'>
+          {article.title}
+        </h2>
         <p className='text-black dark:text-white font-semibold text-justify leading-[140%] tracking-[-0.26px] text-sm md:text-base lg:text-xl'>
           {article.content}
         </p>
