a356f2ffdd944868aa9e7ac519b5ad8fa7629b02
Bruno Martinez <bmartinez.fullstack@gmail.com>
2024-09-12 15:27:20 -0300

    Refactor RatingInput to use hidden input for stars value

--- components/RatePopUp/RatingInput.jsx
+++ components/RatePopUp/RatingInput.jsx
@@ -3,7 +3,7 @@ import StarSvg from '@/components/RatePopUp/StarSvg'
 const RatingInput = ({ stars, starsValue, handleStars }) => {
   return (
     <div>
-      <input type='number' ref={stars} defaultValue={starsValue} />
+      <input type='hidden' ref={stars} defaultValue={starsValue} />
       {[...Array(5)].map((_, i) => (
         <label key={i} htmlFor='stars' data-value={i + 1} onClick={handleStars}>
           {i + 1 <= starsValue ? <StarSvg isActive /> : <StarSvg />}
