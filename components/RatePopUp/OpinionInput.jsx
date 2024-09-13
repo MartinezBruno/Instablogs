231db0dc78e187c186fcb9a062ec736e43b3c614
Bruno Martinez <bmartinez.fullstack@gmail.com>
2024-09-12 22:36:37 -0300

    style(rateus): opinion input

--- components/RatePopUp/OpinionInput.jsx
+++ components/RatePopUp/OpinionInput.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-
 const OpinionInput = ({ handleOpinion }) => {
   return (
-    <div>
-      <input type='text' required onChange={handleOpinion} />
-    </div>
+    <input
+      required
+      type='text'
+      className='w-full h-10 p-2 mt-4 border border-gray-300 rounded-md dark:text-white'
+      onChange={handleOpinion}
+    />
   )
 }
 
