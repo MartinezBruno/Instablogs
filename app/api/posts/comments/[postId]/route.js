a35777e2d21781030a48653d1ea930b26c515184
Bruno Martinez <bmartinez.fullstack@gmail.com>
2025-02-23 17:05:50 -0300

    [chore]: general fixes

--- app/api/posts/comments/[postId]/route.js
+++ app/api/posts/comments/[postId]/route.js
@@ -11,7 +11,7 @@ export const GET = async (request, { params }) => {
     })
     if (!comments) return NextResponse.error(new Error('Comments not found'))
     const commentWithAuthorData = await Promise.all(
-      comments.map(async comment => {
+      comments.map(async (comment) => {
         const author = await prisma.user.findUnique({
           where: {
             id: comment.authorId
@@ -29,6 +29,7 @@ export const GET = async (request, { params }) => {
         }
       })
     )
+
     return NextResponse.json(commentWithAuthorData)
   } catch (error) {
     console.error(error)
@@ -38,7 +39,7 @@ export const GET = async (request, { params }) => {
 
 export const POST = async (request, { params }) => {
   try {
-    const { postId } = params
+    const { postId } = await params
     const body = await request.json()
     const { content, email } = body
 
--- app/blogs/[blogId]/error.jsx
+++ app/blogs/[blogId]/error.jsx
@@ -2,14 +2,15 @@
 
 const error = () => {
   return (
-    <div className='grid place-items-center gap-6 py-40 max-w-[500px] px-8 mx-auto'>
+    <div className='mx-auto grid max-w-[500px] place-items-center gap-6 px-8 py-40'>
       <h1 className='heading1 text-yellow'>Oops, something went wrong buddy</h1>
-      <p className='text-black text-xl leading-[163%]'>
-        Looks like this post doesn&apos;t exist. It may have been deleted or the link is broken. <br />
+      <p className='text-xl leading-[163%] text-black'>
+        Looks like this post doesn&apos;t exist. It may have been deleted or the
+        link is broken. <br />
         Feel free to try again but we can&apos;t promise anything.
       </p>
       <img
-        src='https://khqlvuznkpdsactdscrc.supabase.co/storage/v1/object/sign/Images/Static%20Images/bob%201-min.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvU3RhdGljIEltYWdlcy9ib2IgMS1taW4ucG5nIiwiaWF0IjoxNjkzNTAyODAyLCJleHAiOjI2OTM1MDI4MDF9.j1QKSGVbJxETYmmP-ORjAsz_DGwBNicOx_UlcUbCtiE&t=2023-08-31T17%3A26%3A46.607Z'
+        src='https://firebasestorage.googleapis.com/v0/b/instablogs-82a89.appspot.com/o/5516eb2fe1d9f10587651550f5724757.jpeg?alt=media&token=4fa7f4d9-e4f8-4259-b752-fc73022c4f5a'
         width={500}
         height={'auto'}
         alt='404'
